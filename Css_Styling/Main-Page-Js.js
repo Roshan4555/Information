c416939e3b5e1573266770fbc599a858608e98d1
Roshan <surukutlaroshan@gmail.com>
2020-10-10 15:02:46 +0530

    Responsive change

--- Css_Styling/Main-Page-Js.js
+++ Css_Styling/Main-Page-Js.js
@@ -6,47 +6,40 @@
 				cursor.style.top=p+"px";
 			});
 				function change(){
-					var title1=document.getElementById("mychange");
+					var title1=document.getElementById("background-image-change");
 					title1.src="Images\\Biography.jpg";
 				    var gettitle=document.getElementById("flex-title-display");
                     gettitle.innerHTML="Biography";
-                    // document.getElementById("distitle").style.left="140px";
                     document.getElementById("left").innerHTML="01";
-                    // document.getElementById("sliderid").innerHTML="2";
-				}
+                }
 				function change1(){
-					var title2=document.getElementById("mychange").src="Images\\Technology.jpg";
+					var title2=document.getElementById("background-image-change").src="Images\\Technology.jpg";
 				    var gettitle=document.getElementById("flex-title-display");
                     gettitle.innerHTML="Technology";
-                    // document.getElementById("distitle").style.left="60px";
                     document.getElementById("left").innerHTML="02";   
 				}
 				function change2(){
-					var title3=document.getElementById("mychange").src="Images\\Fitness.jpg";
+					var title3=document.getElementById("background-image-change").src="Images\\Fitness.jpg";
 				    var gettitle=document.getElementById("flex-title-display");
                     gettitle.innerHTML="Fitness";
-                    // document.getElementById("distitle").style.left="340px";
                     document.getElementById("leftvalue").innerHTML="03";
 				}
 				function change3(){
-					var title4=document.getElementById("mychange").src="Images\\Travel.jpg";
+					var title4=document.getElementById("background-image-change").src="Images\\Travel.jpg";
 				    var gettitle=document.getElementById("flex-title-display");
                     gettitle.innerHTML="Travel";
-                    // document.getElementById("distitle").style.left="360px";
                     document.getElementById("leftvalue").innerHTML="04";
 				}
 				function change4(){
-					var title5=document.getElementById("mychange").src="Images\\Hospitality.jpg";
+					var title5=document.getElementById("background-image-change").src="Images\\Hospitality.jpg";
 				    var gettitle=document.getElementById("flex-title-display");
                     gettitle.innerHTML="Hospitality";
-                    // document.getElementById("distitle").style.left="100px";
                     document.getElementById("leftvalue").innerHTML="05";
 				}
 				function change5(){
-					var title6=document.getElementById("mychange").src="Images\\Introspection.jpg";
+					var title6=document.getElementById("background-image-change").src="Images\\Introspection.jpg";
 				    var gettitle=document.getElementById("flex-title-display");
                     gettitle.innerHTML="Processors";
-                    // document.getElementById("distitle").style.left="80px";
                     document.getElementById("leftvalue").innerHTML="06";
 				}
 
